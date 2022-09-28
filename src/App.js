b410bd9e01486f6272cef50df2b2ec53828abab5
MORSHED-HOME <morshed952640@gmail.com>
2022-09-28 22:45:30 +0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>First Input</h1>
+      {/* <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon> */}
+      {/* <FontAwesomeIcon icon={faCart}></FontAwesomeIcon> */}
+    </div>
+  );
+}
+
+export default App;
