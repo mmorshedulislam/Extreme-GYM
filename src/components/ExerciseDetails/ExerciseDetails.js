fd72c22644a9ca18e65ec842919de0576cc8b1d8
MORSHED-HOME <morshed952640@gmail.com>
2022-09-29 07:30:58 +0600

    Profile, Height and Weight, break time and Exercise details design finishd

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -20,22 +20,54 @@ const ExerciseDetails = () => {
       <div className="body">
         <div>
           <h3>
-            75 <span>kg</span>
+            70<span>kg</span>
           </h3>
           <h3>Weight</h3>
         </div>
         <div>
-          <h3>
-            75 <span>kg</span>
-          </h3>
-          <h3>Weight</h3>
+          <h3>5.8</h3>
+          <h3>Height</h3>
         </div>
         <div>
           <h3>
-            75 <span>kg</span>
+            22<span>yrs</span>
           </h3>
-          <h3>Weight</h3>
+          <h3>Age</h3>
         </div>
+      </div>
+      <div className="addABreak">
+        <h2>Add A Break</h2>
+        <div className="addABreakBtn">
+          <button>
+            <span>10</span>s
+          </button>
+          <button>
+            <span>20</span>s
+          </button>
+          <button>
+            <span>30</span>s
+          </button>
+          <button>
+            <span>40</span>s
+          </button>
+          <button>
+            <span>50</span>s
+          </button>
+        </div>
+      </div>
+      <div className="exercise-detail-container">
+        <h2>Exercise Details</h2>
+        <div className="exercise-detail">
+          <h3>Exercise Time</h3>
+          <h3><span>200 </span>seconds</h3>
+        </div>
+        <div className="exercise-detail">
+          <h3>Break Time</h3>
+          <h3><span>15 </span>seconds</h3>
+        </div>
+      </div>
+      <div>
+        <button className="btn-activity">Activity Completed</button>
       </div>
     </div>
   );
